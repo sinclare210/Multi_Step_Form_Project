f1f83634fed4d50d9f33f8c2c5d9dedd33117c65
Sinclair <sinclairolajuwon@gmail.com>
2024-08-30 21:07:37 -0500

    modified Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
    content: ["./src/**/*.{html,js}"],
   theme: {
+    colors:{
+"Marineblue": "hsl(213, 96%, 18%)",
+"Purplishblue": "hsl(243, 100%, 62%)",
+"Pastelblue": "hsl(228, 100%, 84%)",
+"Lightblue": "hsl(206, 94%, 87%)",
+"Strawberryred": "hsl(354, 84%, 57%)",
+"Coolgray": "hsl(231, 11%, 63%)",
+"Lightgray":" hsl(229, 24%, 87%)",
+"Magnolia": "hsl(217, 100%, 97%)",
+"Alabaster":" hsl(231, 100%, 99%)",
+"White": "hsl(0, 0%, 100%)"
+
+    },
+    fontFamily:{
+      ubuntu:["Ubuntu", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
